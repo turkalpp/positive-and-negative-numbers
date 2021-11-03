72597e574e197421bf28db72bc2cf18baeeeeed7
turkalpp <71527921+turkalpp@users.noreply.github.com>
2021-11-03 21:45:55 +0300

    Add files via upload

---
+++ positive and negative numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int x;
+	cout<<" Enter a number: ";
+	cin>>x;
+	
+	if(x<0)
+	{
+		cout<<" Number is negative.";
+	}
+	else
+	{
+		cout<<" Number is positive. ";
+	}
+}
